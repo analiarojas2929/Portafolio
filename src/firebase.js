92d7d619a9827046c06971bfa815da305e992ae7
analiarojas2929 <analia.rojas@duocuc.cl>
2025-03-03 14:57:44 -0300

    commits

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCm3ZMXaeOtJaFDKJbmIGsb0dINCLxA_cE",
-    authDomain: "portafolio-6d91b.firebaseapp.com",
-    projectId: "portafolio-6d91b",
-    storageBucket: "portafolio-6d91b.firebasestorage.app",
-    messagingSenderId: "413982999941",
-    appId: "1:413982999941:web:b4e64baa2dda32eb2570ec",
-    measurementId: "G-N6M66MQZ73"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export default app;
